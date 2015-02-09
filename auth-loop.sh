f52c9041254565606f799a7b09954922440e8915
Robert Quattlebaum <darco@deepdarc.com>
2015-02-08 20:35:52 -0800

    More updates.

--- auth-loop.sh
+++ auth-loop.sh
@@ -8,12 +8,19 @@ access_granted() {
 	echo Access Granted: $AUTH_DN
 }
 
+verify_access() {
+	# TODO: Look up and verify that $AUTH_DN has access to this zone!
+	return 0
+}
 
 while true ;
 do
 	sleep 4
 	if AUTH_DN=`./simple-card-auth.sh`
-	then access_granted
+	then if verify_access
+		then access_granted
+		else access_denied
+		fi
 	else access_denied
 	fi
 done
