8256a2e4683f30d5ac0521d09e76ea11e8a53167
Robert Quattlebaum <darco@deepdarc.com>
2015-02-08 19:50:59 -0800

    more updates

--- auth-loop.sh
+++ auth-loop.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+access_denied() {
+	echo Access Denied: $AUTH_DN
+}
+
+access_granted() {
+	echo Access Granted: $AUTH_DN
+}
+
+
 while true ;
 do
-	sleep 2
-	auth_dn=`./simple-card-auth.sh` || continue
-	echo access granted
+	sleep 4
+	if AUTH_DN=`./simple-card-auth.sh`
+	then access_granted
+	else access_denied
+	fi
 done
