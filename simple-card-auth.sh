79988d8f33515cefb5889dbb8e39163d43afc3ab
Robert Quattlebaum <darco@deepdarc.com>
2015-02-08 19:33:03 -0800

    More updates

--- simple-card-auth.sh
+++ simple-card-auth.sh
@@ -11,8 +11,14 @@ SUBJECT_DN_FILE=subject-dn.tmp
 SERIAL_FILE=serial.tmp
 TMP_FILE=temporary.tmp
 
+CACHE_DIR=`pwd`/card-cache
+
 STRICT_CHECK=0
 KEY_ID=4
+STDERR=/dev/null
+#STDERR=/dev/stderr
+
+export OPENSC_CONF=`pwd`/opensc.conf
 
 cleanup() {
 	rm $CHAL_FILE 2> /dev/null
@@ -26,38 +32,52 @@ cleanup() {
 	rm $TMP_FILE 2> /dev/null
 }
 
+cache_cert() {
+	cache_file="$CACHE_DIR/`cat $SERIAL_FILE | openssl sha1 | cut -d ' ' -f 2`"
+	mkdir -p "$CACHE_DIR"
+	cp "$CERT_FILE" "$cache_file"
+}
+
+cache_lookup() {
+	cache_file="$CACHE_DIR/`cat $SERIAL_FILE | openssl sha1 | cut -d ' ' -f 2`"
+	if [ -f "$cache_file" ]
+	then
+		#echo Cache hit
+		cp "$cache_file" "$CERT_FILE"
+	else return 1
+	fi
+}
+
 die() {
-	echo ""
-	echo "ACCESS DENIED."
+#	echo ""
+#	echo "ACCESS DENIED."
 	cleanup
 	exit 1
 }
 
 #set -x
 
+
 # Get ATR
-opensc-tool -w -a 2> /dev/null | tee $ATR_FILE || die
+opensc-tool -w -a 2> $STDERR > $ATR_FILE || die
 
 # Get Serial
-opensc-tool --serial 2> /dev/null | tee $SERIAL_FILE || die
+opensc-tool --serial 2> $STDERR > $SERIAL_FILE || die
 
 # Extract the certificate
-pkcs15-tool --no-prompt --read-certificate $KEY_ID -o $CERT_FILE 2> /dev/null || die
+( cache_lookup || pkcs15-tool --no-prompt --read-certificate $KEY_ID -o $CERT_FILE 2> $STDERR ) || die
 
 # Verify the certificate
-#openssl verify [-CApath directory] [-CAfile file] [-purpose purpose] [-untrusted file] [-help] [-issuer_checks] [-verbose] [-] [certificates]
 cat $CERT_FILE | openssl verify -CAfile ca.crt -verbose -purpose sslclient > $TMP_FILE || die
 
 [ $STRICT_CHECK = 1 ] && [ "`cat $TMP_FILE`" '!=' "stdin: OK" ] && die
 
-# Print out the subject name
-openssl x509 -in $CERT_FILE -nameopt RFC2253 -noout -subject | tee $SUBJECT_DN_FILE || die
 
 # Extract the public key
 openssl x509 -pubkey -noout -in $CERT_FILE > $PUB_KEY_FILE || die
 
 # Calculate a challenge
-dd if=/dev/random of=$CHAL_FILE bs=32 count=1 2> /dev/null || die
+dd if=/dev/random of=$CHAL_FILE bs=32 count=1 2> $STDERR || die
 
 # Calculate the hash of the challenge
 openssl sha -sha256 -binary < $CHAL_FILE > $CHALHASH_FILE || die
@@ -66,16 +86,21 @@ openssl sha -sha256 -binary < $CHAL_FILE > $CHALHASH_FILE || die
 if ( openssl x509 -in cert.pem -text | grep -q -s "Signature Algorithm: ecdsa" ) ;
 then
 	# ECDSA signatures need to be converted to DER format.
-	pkcs15-crypt -s -k $KEY_ID --sha-256 -i $CHALHASH_FILE -o $SIG2_FILE 2> /dev/null || die
+	pkcs15-crypt -s -k $KEY_ID --sha-256 -i $CHALHASH_FILE -o $SIG2_FILE 2> $STDERR || die
 	./ecdsa-pkcs11-to-asn1 < $SIG2_FILE > $SIG_FILE || die
 else
-	pkcs15-crypt -s -k $KEY_ID --sha-256 --pkcs1 -i $CHALHASH_FILE -o $SIG_FILE 2> /dev/null || die
+	pkcs15-crypt -s -k $KEY_ID --sha-256 --pkcs1 -i $CHALHASH_FILE -o $SIG_FILE 2> $STDERR || die
 fi
 
 # Verify the response
-openssl dgst -sha256 -verify $PUB_KEY_FILE -signature $SIG_FILE $CHAL_FILE || die
+openssl dgst -sha256 -verify $PUB_KEY_FILE -signature $SIG_FILE $CHAL_FILE 2>&1 > $STDERR || die
 
-echo ""
-echo "ACCESS GRANTED."
+# Print out the subject name
+openssl x509 -in $CERT_FILE -nameopt RFC2253 -noout -subject | sed 's:^[^ ]* ::' |  tee $SUBJECT_DN_FILE || die
+
+cache_cert
+
+#echo ""
+#echo "ACCESS GRANTED."
 cleanup
 exit 0
